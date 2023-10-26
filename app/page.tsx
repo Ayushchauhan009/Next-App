9509cfca7b5177a5233a7eef544744083dbcaaa9
aayushchauhan10 <ai.aayush10@gmail.com>
2023-10-26 15:34:15 +0530

    commiting n+1 time

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 
 export default function Home() {
+  const selectedFilePath = "/img-1.png";
+
+  // Convert the file path to a File object
+  const selectedFile = new File([selectedFilePath], 'img-1.png');
   return (
     <div className=" ">
-    <Hero />
+    <Hero selectedFile={selectedFile}/>
    
     </div>
   )
