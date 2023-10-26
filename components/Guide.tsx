7917a5e4a1d167136d2c4f0c521f8b4c53c8f10a
aayushchauhan10 <ai.aayush10@gmail.com>
2023-10-26 15:52:43 +0530

    commiting n+1 time

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -11,7 +11,7 @@ const Guide = () => {
      <div className='w-[530px] xxl:w-[630px] 2xl:w-[700px] dropShadow rounded-[8px] h-[591px] flex flex-col items-center justify-center text-center font-bold text-[43px]'>
       <h1>Output</h1> 
       <div id='image-container'>
-      <Image src="/img-1.png" alt='person1' width={300} height={300} id='imageID'/> 
+      <Image src="/testImage.jpg" alt='person1' width={600} height={600} id='imageID' className='w-full h-full px-1'/> 
       </div>
       </div>
       <div className='pt-[22px]'>
