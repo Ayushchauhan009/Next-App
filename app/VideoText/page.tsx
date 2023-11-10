7c5f08f5245edc12ce8d96167c221186e27ea829
aayushchauhan10 <ai.aayush10@gmail.com>
2023-11-10 18:22:06 +0530

    commiting again

--- app/VideoText/page.tsx
+++ app/VideoText/page.tsx
@@ -1,17 +1,30 @@
 "use client"
 
+
 import Timeline from '@/components/Timeline'
 import Filters from '@/components/Video/Filters'
 import GenerateAud from '@/components/Video/GenerateAud'
 import OutputVideo from '@/components/Video/OutputVideo'
-import React from 'react'
+import React, {useState} from 'react'
+
+
+
+
 
 const page = () => {
+
+
+  const [videoData, setVideoData] = useState<File | undefined>(undefined);
+  const handleVideoUpload = (video: File) => {
+    setVideoData(video);
+  };
   return (
     <div className='max-container flexOne flex-col padding-container2 gap-x-10 pt-10 pb-[10px] md:gap-x-10 lg:pt-10'>
       <div className='flex space-x-5'>
-      <GenerateAud />
-      <OutputVideo />
+      <GenerateAud onVideoUpload={handleVideoUpload}/>
+      <div className='w-[570px] xxl:w-[660px] 2xl:w-[820px] dropShadow rounded-[8px] h-[669px] my-auto  text-center font-bold text-[43px]'>
+      <OutputVideo video={videoData}/>
+      </div>
       <Filters />
       </div>
       <Timeline />
